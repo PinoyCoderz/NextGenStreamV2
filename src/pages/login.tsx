fa3cb0e2c3b300e31281d76c6a6adb8c03974c90
PinoyCoderz <103308907+PinoyCoderz@users.noreply.github.com>
2024-09-28 20:25:55 +0800

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -28,7 +28,7 @@ const LoginPage = () => {
           data-tooltip-id="tooltip"
           data-tooltip-content="Rive"
         />
-        <p>Your Personal Streaming Oasis</p>
+        <p>DXStream App Source</p>
       </div>
       <div className={styles.settings}>
         <h1>Login</h1>
